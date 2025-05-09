6d7f23dc8cb36392fb70be28c57317e544124f82
imsat <satyajit.cse@yahoo.com>
2025-05-10 04:22:43 +0600

    prod build

--- resources/js/pages/Navbar.vue
+++ resources/js/pages/Navbar.vue
@@ -87,7 +87,7 @@ onUnmounted(() => {
                             <a href="#" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100" role="menuitem">Settings</a>
                             <button
                                 @click="store.logout"
-                                class="block w-full px-4 py-2 text-left text-sm text-gray-700 hover:bg-gray-100"
+                                class="block w-full px-4 py-2 text-left text-sm text-gray-700 hover:bg-gray-100 cursor-pointer"
                                 role="menuitem"
                             >
                                 Sign out
